88f9d6b99f0babd9abb00a4151f9d85462fa2907
lshaluminum <lshaluminum@163.com>
2017-12-15 16:15:18 +0800

    substitue favicon.ico && amend footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
-                    <span>蜀ICP备16008148号-2</span> |
+                    <span>暂无备案</span> |
                     <span>Powered by <a href="https://github.com/lufficc/Xblog">Xblog</a> </span>|
                     <a href="{{ url('feed.xml') }}" rel="feed" type="application/rss+xml"
                        title="Feed"><img width="13"
